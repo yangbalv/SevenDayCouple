408be8e510b1e2e8557caccc1b8a9a4af16fcdba
张天一 <zhangtianyi@everelegance.com.cn>
2023-04-24 15:30:12 +0800

    项目搭建-初始文件

---
+++ src/main/java/com/jiuyuetian/sevendaycouple/SevenDayCoupleApplication.java
@@ -0,0 +1,13 @@
+package com.jiuyuetian.sevendaycouple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenDayCoupleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevenDayCoupleApplication.class, args);
+    }
+
+}
