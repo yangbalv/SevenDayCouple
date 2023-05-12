5b23b18d326002a9462843dd969b212194980978
张天一 <zhangtianyi@everelegance.com.cn>
2023-05-12 15:57:04 +0800

    项目配置多源数据库并且实现多源数据库的数据可用

--- src/main/java/com/jiuyuetian/sevendaycouple/controller/LoginController.java
+++ src/main/java/com/jiuyuetian/sevendaycouple/controller/LoginController.java
@@ -1,8 +1,12 @@
 package com.jiuyuetian.sevendaycouple.controller;
 
 import com.jiuyuetian.sevendaycouple.dto.LoginRequestDto;
+import com.jiuyuetian.sevendaycouple.mappers.mybatiss1.UserMapper;
+import com.jiuyuetian.sevendaycouple.mappers.mybatiss2.BackUserMapper;
+import com.jiuyuetian.sevendaycouple.model.UserModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +14,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
     public static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
+    @Autowired
+    private UserMapper userMapper;
+
+
+    @Autowired
+    private BackUserMapper backUserMapper;
+
     @RequestMapping("/login")
     public String login(LoginRequestDto requestDto) {
         logger.info("/login:[{}]", requestDto);
+        UserModel userModel = userMapper.getOneByUserName(requestDto.getUserName());
+        logger.info("userModel is:[{}]", userModel.toString());
         return requestDto.toString();
     }
+
+    @RequestMapping("/register")
+    public String register(LoginRequestDto requestDto) throws Exception {
+        logger.info("/register:[{}]", requestDto);
+        UserModel userModel = userMapper.getOneByUserName(requestDto.getUserName());
+        if (null == userModel) {
+            userModel = new UserModel();
+            userModel.setUserName(requestDto.getUserName());
+            userModel.setPassword(requestDto.getPassword());
+            userMapper.insert(userModel);
+            backUserMapper.insert(userModel);
+            return "添加成功";
+        } else {
+            logger.info("userModel is:[{}]", userModel.toString());
+            return "用户以及被注册";
+        }
+    }
+
 }
--- src/main/java/com/jiuyuetian/sevendaycouple/mappers/mybatiss2/BackUserMapper.java
+++ src/main/java/com/jiuyuetian/sevendaycouple/mappers/mybatiss2/BackUserMapper.java
@@ -1,8 +1,8 @@
-package com.jiuyuetian.sevendaycouple.mappers.mybatiss1;
+package com.jiuyuetian.sevendaycouple.mappers.mybatiss2;
 
 import com.jiuyuetian.sevendaycouple.base.dao.IBaseDao;
 import com.jiuyuetian.sevendaycouple.model.UserModel;
 
-public interface UserMapper extends IBaseDao<UserModel, String> {
+public interface BackUserMapper extends IBaseDao<UserModel, String> {
     UserModel getOneByUserName(String userName);
 }
